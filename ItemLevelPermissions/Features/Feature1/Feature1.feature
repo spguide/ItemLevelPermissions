90c21b9bd7bdc546428e53d9e2ac76d0cd64ae28
spprashaant <thesharepointguide@gmail.com>
2016-03-04 21:07:41 +0530

    v1.0

---
+++ ItemLevelPermissions/Features/Feature1/Feature1.feature
@@ -0,0 +1,9 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="6d6a95b4-f533-4a8c-aeb2-cb4595e12e01" description="SharePoint App Feature" featureId="6d6a95b4-f533-4a8c-aeb2-cb4595e12e01" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="ItemLevelPermissions Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="59fb0bce-95de-499c-b0c6-0f06fab69347" />
+    <projectItemReference itemId="cea35a42-9116-4dd1-b6f9-d06cbb26b9c3" />
+    <projectItemReference itemId="018027be-99a1-454c-b370-77f93e8e29d4" />
+    <projectItemReference itemId="da406714-55d2-4fd3-b4a5-5b6074200c4c" />
+  </projectItems>
+</feature>
---
+++ ItemLevelPermissions/pkg/Debug/ItemLevelPermissions/ItemLevelPermissions_Feature1/Scripts/App.js
@@ -0,0 +1,84 @@
+﻿'use strict';
+
+var hostweburl;
+var appweburl;
+var executor;
+
+$(document).ready(function () {
+  getCustomPermission();
+});
+
+// retrieve the hostweb url and appweb url from the query string
+function getCustomPermission() {
+  $(document).ready(function () {
+    hostweburl =
+        decodeURIComponent(
+            getQueryStringParameter("SPHostUrl")
+    );
+    appweburl =
+        decodeURIComponent(
+            getQueryStringParameter("SPAppWebUrl")
+    );
+
+    var scriptbase = hostweburl + "/_layouts/15/";
+
+    $.getScript(scriptbase + "SP.RequestExecutor.js", getRoleAssignments);
+  });
+}
+
+//make the rest call
+function getRoleAssignments() {
+  executor = new SP.RequestExecutor(appweburl);
+
+  executor.executeAsync(
+      {
+        url:
+            appweburl +
+            "/_api/SP.AppContextSite(@target)/web/lists/getbytitle('Test')/items(1)?$expand=RoleAssignments/Member/Users&@target='" +
+            hostweburl + "'",
+        method: "GET",
+        headers: { "Accept": "application/json; odata=verbose" },
+        success: successHandler,
+        error: errorHandler
+      }
+  );
+}
+
+//display results
+function successHandler(data) {
+  var jsonObject = JSON.parse(data.body);
+  var roleAssignmentHTML = "";
+
+  var results = jsonObject.d.RoleAssignments.results;
+  for (var i = 0; i < results.length; i++) {
+
+    roleAssignmentHTML = roleAssignmentHTML +
+          "<p><b>" + results[i].Member.OwnerTitle +
+          "</b></p>";
+    var users = results[i].Member.Users.results;
+    if (users) {
+      for (var u = 0; u < users.length; u++) {
+        roleAssignmentHTML = roleAssignmentHTML + "<p>" + users[u].Title + "</p>";
+      }
+    }
+  }
+  document.getElementById("message").innerHTML =
+      roleAssignmentHTML;
+}
+
+
+function errorHandler(data, errorCode, errorMessage) {
+  document.getElementById("message").innerText =
+      "Could not complete cross-domain call: " + errorMessage;
+}
+
+function getQueryStringParameter(paramToRetrieve) {
+  var params =
+      document.URL.split("?")[1].split("&");
+  var strParams = "";
+  for (var i = 0; i < params.length; i = i + 1) {
+    var singleParam = params[i].split("=");
+    if (singleParam[0] == paramToRetrieve)
+      return singleParam[1];
+  }
+}
